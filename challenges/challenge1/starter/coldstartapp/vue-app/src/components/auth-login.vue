3f65fa025d948404b2e1b0cd080ad65a25ee1e6f
Dries Peeters <dries.peeters.1990@gmail.com>
2021-03-03 20:13:12 +0100

    correct login

--- challenges/challenge1/starter/coldstartapp/vue-app/src/components/auth-login.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/components/auth-login.vue
@@ -19,5 +19,5 @@ export default {
 </script>
 
 <template>
-  <div class="auth-link" @click="goAuth">{{ provider }}</div>
+  <div class="auth-link" @click="goAuth">Login with {{ provider }}</div>
 </template>
